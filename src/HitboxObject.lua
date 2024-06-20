8420f0a230e5486d92c916b6c83de03ed303cbc0
Jahames <75079031+AnUnderratedPerson@users.noreply.github.com>
2024-06-20 14:54:40 -0500

    new files

---
+++ src/HitboxObject.lua
@@ -0,0 +1,131 @@
+--[[
+    HitboxObject.lua
+    Author: Conscience (@MixedConscience)
+    -----------------
+    Allows for raycast hitboxes to be created.
+]]
+
+local RunService = game:GetService("RunService")
+
+local HitboxObject = {}
+HitboxObject.__index = HitboxObject
+
+local Visualizer = Instance.new("Part")
+Visualizer.Anchored = true
+Visualizer.CanCollide = false
+Visualizer.Material = Enum.Material.Neon
+
+export type Array<V> = { V }
+export type Map<K, V> = { [K]: V }
+
+export type CastOptions = {
+    Object: Instance,
+	DebugMode: boolean?,
+	Attachments: Array<Attachment>?,
+}
+
+--// INITIALIZE
+
+function HitboxObject.new(options: CastOptions)
+	local self = setmetatable({}, HitboxObject)
+
+    self.Object = options.Object
+    self.InDebugMode = options.DebugMode or false
+	self.AttachmentPoints = options.Attachments or {}
+    self.RegisteredHitCallback = nil
+    self.RunServiceEvent = nil
+	self.ShouldCast = true
+
+	return self
+end
+
+function HitboxObject:Initialize()
+end
+
+function HitboxObject:Destroy()
+    self:DisableCasting()
+
+    self.Object = nil
+    self.AttachmentPoints = {}
+    setmetatable(self, nil)
+end
+
+--// PRIVATE
+
+function HitboxObject:_CastRayWithPositions(from: Vector3, to: Vector3, raycastParams: RaycastParams)
+    local Object = self.Object
+	local CastedRay = workspace:Raycast(from, to, raycastParams)
+
+	if self.InDebugMode then
+		task.spawn(function()
+			local CastVisualizationFolder = Instance.new("Folder")
+            CastVisualizationFolder.Name = "__CastVisualization"
+            CastVisualizationFolder.Parent = Object
+
+			local VisualizationPart = Visualizer:Clone()
+			VisualizationPart.CFrame = CFrame.lookAt(from, to) * CFrame.new(0.1, 0.1, -(to - from).Magnitude/2)
+			VisualizationPart.Size = Vector3.new(0.05, 0.05, (from - to).Magnitude)
+			VisualizationPart.Parent = CastVisualizationFolder
+
+			task.delay(2, VisualizationPart.Destroy, VisualizationPart)
+		end)
+	end
+
+	return CastedRay
+end
+
+--// PUBLIC
+
+function HitboxObject:RegisterHitCallback(callback: (RaycastResult) -> (boolean?))
+    self.RegisteredHitCallback = callback
+end
+
+function HitboxObject:EnableCasting(ignoreDescendants: { Instance }?)
+    ignoreDescendants = ignoreDescendants or {}
+    table.insert(ignoreDescendants :: {}, self.Object)
+
+	local IgnoreRaycastParams = RaycastParams.new()
+    IgnoreRaycastParams.FilterType = Enum.RaycastFilterType.Exclude
+    IgnoreRaycastParams.FilterDescendantsInstances = ignoreDescendants
+
+	local PreviousAttachments = {}
+    local RunServiceEvent = if RunService:IsClient() then RunService.RenderStepped else RunService.Heartbeat
+
+    self.RunServiceEvent = RunServiceEvent:Connect(function(delta)
+        if not self.ShouldCast then
+			return
+		end
+
+		for _, attachment: Attachment in self.AttachmentPoints do
+			local LastPosition = PreviousAttachments[attachment.Name]
+
+			if LastPosition and (LastPosition ~= attachment.WorldPosition) then
+                local CurrentPosition = attachment.WorldPosition
+				local CastedRay: RaycastResult = self:_CastRayWithPositions(LastPosition, CurrentPosition, IgnoreRaycastParams)
+
+				if CastedRay and self.RegisteredHitCallback then
+					local SuspendCasting = self.RegisteredHitCallback(CastedRay)
+
+					if SuspendCasting then
+                        self:DisableCasting()
+						return
+					end
+				end
+			end
+
+            PreviousAttachments[attachment.Name] = attachment.WorldPosition
+		end
+    end)
+end
+
+function HitboxObject:DisableCasting()
+	self.ShouldCast = false
+    self.RunServiceEvent = if self.RunServiceEvent then self.RunServiceEvent:Disconnect() else nil
+
+    --> note:
+    --> instances may have been created in the debug version of this object
+    --> they will usually get removed after a certain time, so we dont worry about them
+    return true
+end
+
+return HitboxObject
---
+++ src/RagdollObject.lua
@@ -0,0 +1,194 @@
+--[[
+    RagdollObject.lua
+    Author: Conscience (@MixedConscience)
+    -----------------
+    Allows for R6 players to be ragdolled. 
+    This functionality can be enabled and disabled quickly throughout gameplay.
+]]
+
+local RagdollObject = {}
+RagdollObject.__index = RagdollObject
+
+-----------------------------------
+--> Constructor/Destructor
+-----------------------------------
+
+function RagdollObject.new(character: Model)
+	local self = setmetatable({}, RagdollObject)
+
+    self.Character = character
+    self.Humanoid = character:WaitForChild("Humanoid")
+    self.InRagdolledState = false
+	
+	return self
+end
+
+function RagdollObject:Initialize()
+    local Character: Model = self.Character
+    local Humanoid: Humanoid = self.Humanoid
+    
+    Humanoid.BreakJointsOnDeath = false
+    Humanoid.RequiresNeck = false
+
+    --> Folder to hold the colliders in the character
+    --> These are removed when the character despawns or when the object is destroyed
+    local LimbCollisionFolder = Instance.new("Folder")
+	LimbCollisionFolder.Name = "__LimbCollision"
+	LimbCollisionFolder.Parent = Character
+
+    --> note:
+    --> having colliders prevent the limbs from falling into the ground below the character
+    --> if we simply enable the collision on the actual limbs, that may lead to stiff looking arms and legs
+    --> therefore, having colliders will make it seem more realistic in a sense
+    local SizeOfColliders = {
+        ["Head"]        = Vector3.new(0.8, 0.8, 0.8),
+        ["Left Arm"]    = Vector3.new(0.8, 1.7, 0.8),
+        ["Right Arm"]   = Vector3.new(0.8, 1.7, 0.8),
+        ["Left Leg"]    = Vector3.new(0.8, 1.2, 0.8),
+        ["Right Leg"]   = Vector3.new(0.8, 1.2, 0.8),
+    }
+
+    --> Initializing and setting the size of the colliders within the character
+	for limbName, size in SizeOfColliders do		
+		local Limb = Character:FindFirstChild(limbName)
+
+        if not Limb then
+            warn(`Character<{Character.Name}>: Cannot locate limb {limbName}`)
+            continue
+        end
+
+        local ClonedLimb = Limb:Clone()
+        local JoinWeld = Instance.new("Weld")
+		
+		JoinWeld.Name = limbName
+		JoinWeld.Part0 = Limb
+		JoinWeld.Part1 = ClonedLimb
+		JoinWeld.Parent = ClonedLimb
+		
+		ClonedLimb.Size = size
+		ClonedLimb.CanCollide = true
+		ClonedLimb.Transparency = 1
+		ClonedLimb.Parent = LimbCollisionFolder
+	end
+
+    --> note:
+    --> we return self for functionality such as: `local obj = Ragdoll.new(char):Initialize()`
+    return self
+end
+
+function RagdollObject:Destroy()
+    if self.InRagdolledState then
+        self:DisableRagdoll()
+    end
+
+    local Character: Model = self.Character
+    local LimbCollisionFolder: Folder = Character:FindFirstChild("__LimbCollision") :: Folder
+
+    if LimbCollisionFolder then
+        LimbCollisionFolder:ClearAllChildren()
+        LimbCollisionFolder:Destroy()  
+    end
+
+    self.Character = nil
+    self.Humanoid = nil
+    setmetatable(self, nil)
+end
+
+-----------------------------------
+--> Public Member Functions
+-----------------------------------
+
+function RagdollObject:EnableRagdoll(): boolean
+    if self.InRagdolledState then
+        return false
+    end
+
+    self.InRagdolledState = true
+    
+    local Character: Model = self.Character
+    local Humanoid: Humanoid = self.Humanoid
+
+    --> note:
+    --> This table is used to attain the correct positions for the attachments for the respective limbs
+    --> If the limb attachments arent corrected, they can lead to unpleasing ragdoll effects
+	local CorrectAttachmentPositions = {
+		["Left Hip"] = {
+			Att0Position = Vector3.new(-0.5, -1, 0),
+			Att1Position = Vector3.new(0, 1, 0)
+		},
+
+		["Right Hip"] = {
+			Att0Position = Vector3.new(0.5, -1, 0),
+			Att1Position = Vector3.new(0, 1, 0)
+		},
+	}
+
+	Humanoid:SetStateEnabled(Enum.HumanoidStateType.GettingUp, false)
+	Humanoid:ChangeState(Enum.HumanoidStateType.Physics)
+	Humanoid.PlatformStand = true
+
+	for _, motor in Character:GetDescendants() do
+		if motor:IsA("Motor6D") then
+			local Part0 = motor.Part0
+			local Part1 = motor.Part1
+
+            --> Making new attachments for the ball socket connection
+            --> We do this to allow the limb to move freely while attached to the character
+			local Attachment0 = Instance.new("Attachment")
+            Attachment0.Name = "__RagdollAttachment"
+			Attachment0.Position = motor.C0.Position
+			Attachment0.Parent = Part0
+
+			local Attachment1 = Instance.new("Attachment")
+            Attachment1.Name = "__RagdollAttachment"
+			Attachment1.Position = motor.C1.Position
+			Attachment1.Parent = Part1
+
+			local BallSocket = Instance.new("BallSocketConstraint")
+			BallSocket.Attachment0 = Attachment0
+			BallSocket.Attachment1 = Attachment1
+
+			local AttachmentPositions = CorrectAttachmentPositions[motor.Name]
+
+			if AttachmentPositions then
+				Attachment0.Position = AttachmentPositions.Att0Position
+				Attachment1.Position = AttachmentPositions.Att1Position
+			end
+
+			BallSocket.Parent = Part0
+			motor.Enabled = false
+		end
+	end
+
+    return true
+end
+
+function RagdollObject:DisableRagdoll(): boolean
+    if not self.InRagdolledState then
+        return false
+    end
+
+    self.InRagdolledState = false
+
+    local Character: Model = self.Character
+    local Humanoid: Humanoid = self.Humanoid
+
+	for _, instance in Character:GetDescendants() do
+		if instance:IsA("BallSocketConstraint") then
+			local Attachment0, Attachment1 = instance.Attachment0, instance.Attachment1
+			if Attachment0 then Attachment0:Destroy() end
+			if Attachment1 then Attachment1:Destroy() end
+
+			instance:Destroy()
+        elseif instance:IsA("Motor6D") then
+            instance.Enabled = true
+		end
+	end
+
+	Humanoid.PlatformStand = false
+	Humanoid:SetStateEnabled(Enum.HumanoidStateType.GettingUp, true)
+
+    return true
+end
+
+return RagdollObject
